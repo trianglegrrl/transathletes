be0dd1d2230d0d823ebe37e5d5ff2df7c4c131a8
Alaina Hardie <alaina@Alainas-MacBook-Pro.local>
2013-04-15 06:45:53 -0500

    Added new files

---
+++ faq.php
@@ -0,0 +1,60 @@
+<?php
+	include 'header.php';
+?>
+<!-- begin faq -->
+
+<div class="row-fluid">
+	<div class="span10 offset1">
+		<h1>Frequently asked questions</h1>
+		<p>Bored? In a hurry? Short attention span? Here are brief answers to some of the most frequently asked questions about transsexual athletes.</p>
+		<p class="faq-question">
+			I have, like, 60 seconds. Summarize your argument for allowing transsexual women to compete in women's divisions.
+		</p>
+		<p class="faq-answer">
+			After surgery and a few years of hormone therapy, transsexual women are physiologically female. The advantages yielded by high levels of testosterone are reversed, and transsexual women usually have lower testosterone than non-transsexual women because their body does not contain any structure that produces a lot of testosterone. The relevant genes on the Y chromosome are usually not activated. Any other physical variation (body composition, skeleton shape, etc.) accumulated by puberty falls within the standard variation in the female gender. For sports where body types offer an advantage, they are already divided to accommodate that (usually by weight class).
+		</p>
+		<p class="faq-answer">For these reasons, almost every international sports governing body has already adopted a trans inclusion policy that requires the removal of gonads and a couple of years of hormone therapy. After that, transsexual women compete safely and fairly with other women.
+		</p>
+		<p class="faq-question">
+			Didn't the IOC make their policy for reasons of political correctness? There are no studies confirming the safety of transsexual women competing with non-transsexual women.
+		</p>
+		<p class="faq-answer">
+			This is an absurd claim that has been repeated by many people with no evidence to support it. The IOC policy was created by a group of seven physicians and researchers with expertise in relevant fields, not laypeople who just really, really like transsexual folks. The changes undergone by transsexual women after hormone therapy are well understood and well researched. They don't need studies to specifically test safety because the primary physiological advantage--high levels of testosterone--has been eliminated.</p>
+		<p class="faq-answer">
+			Race is a touchy issue, but there are many physical traits that members of a genetic group share that can provide advantages in sport. Did you know that several decades ago, some people argued for separate divisions for black athletes because they were consistently outperforming white athletes? That hasn't changed--the top athletes in many, many sports are predominantly of recent African descent, within the last 200-300 years--but there are no separate divisions for white and black athletes. This is because the idea does not stand up to scrutiny.</p>
+		<p class="faq-question">
+			Aren't transsexual women just men who cut their penises off?
+		</p>
+		<p class="faq-answer">
+			No. Transsexual women usually (but not always) have <a href="genetics.php">a Y chromosome</a>. Their gender identity and gender presentation are female. Transsexual women have a few options for surgery, some of which are very limited based on their health care coverage. Many of them undergo removal of their testes. Some of them have a surgery that creates female genitalia. Almost all of them undergo extensive hormone therapy to suppress testosterone (an androgen) and other male hormones, and to increase their level of estradiol (an estrogen). Physiologically, this makes their bodies very similar to non-transsexual women who have had their ovaries removed.
+		</p>
+		<p class="faq-question">
+			What should I call a transsexual woman? He? She? It?
+		</p>
+		<p class="faq-answer">
+			It is extremely impolite--wait, let's call it out; it's a giant asshole move, and an expression of deliberate ignorance and cruelty--to refer to a transsexual woman as "he" or "it". <strong>A transsexual woman should be referred to as "she" or "her", just like any other woman.</strong>
+		</p>
+		<p class="faq-question">
+			Don't most organizations ban transsexual women from competing with other women?
+		</p>
+		<p class="faq-answer">
+			Exactly opposite, in fact. The IOC, FILA, the NCAA, the Association of Boxing Commissions, the LPGA, and several state and provincial athletic organizations have trans inclusion policies, including in contact/combat sports like hockey, MMA, boxing, wrestling, and judo. This is because the organizations recognize that transsexual women have bodies that are physiologically female. Usually, for trans women, the policy requires that they have genital surgery that removes their testes, and undergo estrogen therapy for a minimum of two years after surgery. This gives the body time to eliminate the significant advantages that the presence of testosterone offers. Testosterone is a performance-enhancing substance. Estrogens are not.
+		</p>
+		<p class="faq-question">
+			Doesn't the skeleton of a transsexual woman give her physical advantages over
+			a non-transsexual woman?
+		</p>
+		<p class="faq-answer">
+			What advantages would those be? Specifically, what skeletal dimensions do only XY people have that XX people never have? There are none. There is <a href="skeletal.php">tremendous variation within a gender's skeletal dimensions</a>.
+		</p>
+		<p class="faq-answer">
+			For some reason people are quite fixated on bone density. It is not clear how bone density improves performance in most sports, but people latch onto it because it is a difference between XX and XY people. XY people generally have a higher bone mineral density than XX people after going through puberty. However, estrogens also preserves bone density, which is why post-menopausal women take estradiol to help prevent arthritis. And athletes who do any resistance training (load-bearing movements like weightlifting) of both genders have excellent bone density. </p>
+			<p class="faq-answer">Without testosterone, bone density will decrease after several years, giving transsexual women a similar bone density to non-transsexual women who have similar resistance training experience. But again, there is no research showing that a higher bone density improves sport performance unless you are comparing to a person with an osteoporotic skeleton... in which case the person with osteoporosis should be doing weight training and maybe taking estrogen to retain their bone density.
+		</p>
+	</div>
+	</div>
+</div>
+<!-- end faq -->
+<?php
+	include 'footer.php';
+?>
