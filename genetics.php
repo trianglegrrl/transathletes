1b0b7310151709fe5a74e5cc613a07d36b83205f
Alaina <alainahardie@gmail.com>
2013-04-15 08:06:27 -0500

    Added details for Y chromosome genes

--- genetics.php
+++ genetics.php
@@ -14,13 +14,13 @@
 
 		<p>In humans, you get one copy of each chromosome from each of your parents. The egg from your mother carries 23 chromosomes, and so does the sperm from your father that fertilizes the egg and leads to somebody like you. Total: 46 chromosomes.</p>
 
-		<p>In 22 of the 23 chromosomes, you just get one copy from your mother and one from your father. The sex chromosomes, X and Y, are the exception. You always get an X chromosome from your mother, because women who can bear children do not have Y chromosomes: they have one X chromosome from their mother, and one from their father. Men who can father children always have a Y chromosome. So, when your father's body created the sperm with just half of the chromosomes of a normal cell, there's something like a 50% chance that the sperm has an X chromosome, and a 50% chance that it has a Y chromosome. Since only one sperm can fertilize an egg--and this, again, has exceptions--you either end up with a child that has two X chromosomes (one from mom and one from dad) or one X and one Y chromosome (one each from mom and dad).</p>
+		<p>In 22 of the 23 chromosomes, you just get one copy from your mother and one from your father. The sex chromosomes, X and Y, are the exception. You always get an X chromosome from your mother, because women who can bear children do not have Y chromosomes: they have one X chromosome from their mother, and one from their father. Men who can father children always have a Y chromosome. So, when your father's body created the sperm with just half of the chromosomes of a normal cell, there's something like a 50% chance that the sperm has an X chromosome, and a 50% chance that it has a Y chromosome. Since only one sperm can fertilize an egg--and this, again, has exceptions--you either end up with a child that has two X chromosomes (one from mom and one from dad) or one X and one Y chromosome (one each from mom and dad). The genetic shorthand for this is 46,XX and 46,XY (46 total chromosomes, plus the sex-specific genes).</p>
 
 		<p>The Y chromosome is very tiny compared to the other chromosomes, and as far as we can tell it carries relatively few genes when compared to powerhouse chromosomes like the X. The primary responsibility of the Y chromosome genes is for the production and sustenance of the male body. It doesn't do much else. When you think about it, with the only difference being the tiny Y chromosome, XX and XY people are far more alike than they are different.</p>
 		
-		<p>The plot thickens! Chromosomal sex is not as simple as XX and XY. There are genetic variations like XXY, XYY, and XXXY. There are also XY people who have some broken genes on the Y chromosome that mean that their bodies do not respond to testosterone, so they are chromosomally XY but never develop physically like a male. You can see how it gets sticky. Estimates are that as many as 1 percent of humans are neither XX nor XY. With very few exceptions, the most you can say about genetics is that your genes and related genetic material has a certain percentage chance of creating a trait in a body.</p>
+		<p>The plot thickens! Chromosomal sex is not as simple as 46,XX and 46,XY. There are genetic variations like XXY, XYY, and XXXY. There are also 46,XY people who have some broken genes on the Y chromosome that mean that their bodies do not respond to testosterone, so they are chromosomally 46,XY but never develop physically like a male. You can see how it gets sticky. Estimates are that as many as 1 percent of humans are neither 46,XX nor 46,XY. With very few exceptions, the most you can say about genetics is that your genes and related genetic material has a certain percentage chance of creating a trait in a body.</p>
 
-		<p>Most transsexual women have an XY karotype, and most develop male characteristics. However, science's best informed guess is that at some point during their development--either before birth or during early childhood--the environment causes something to "break" in the brain, forming a female gender identity and a male body.</p>
+		<p>Most transsexual women have an 46,XY karotype, and most develop male characteristics. However, science's best informed guess is that at some point during their development--either before birth or during early childhood--the environment causes something to "break" in the brain, forming a female gender identity and a male body.</p>
 
 		<p>A few Y chromosome genes are very active during development. The most well-known gene is called SRY: sex-determining region Y. Physically, early in development before birth, we start with the same bits. Around six weeks after conception, the SRY gene activates and starts to create testes instead of ovaries.</p>
 		
@@ -28,9 +28,46 @@
 
 		<p>One of the genes you have produces a protein called an androgen receptor. Androgens are the "male" hormones, like testosterone and DHT, and receptors are like little locks that are open and activated when the testosterone key is inserted. You have other genes that create enzymes--little molecules that help biochemical reactions happen--that are responsible for converting other steroids to testosterone. With the combination of the lock (the androgen receptor) and the key (the androgen like testosterone), testosterone can work its magic, and cause the transcription of other genes. That's how testosterone does its job.</p>
 		
-		<p>So your genes are not deterministic. Without testosterone, the genes that are activated by testosterone are not active. So if you remove the major source of testosterone--which transsexual women do when they remove their testes and go on hormone replacement therapy--the body becomes physiologically female. There is little use for the Y chromosome at all. It basically goes dormant. And since your cells do replace themselves as you age, and </p>
+		<p>So your genes are not deterministic. Without testosterone, the genes that are activated by testosterone are not active. So if you remove the major source of testosterone--which transsexual women do when they remove their testes and go on hormone replacement therapy--the body becomes physiologically female. There is little use for the Y chromosome at all. It basically goes dormant.</p>
+
+		<p>There are a little less than a hundred genes on the Y chromosome that code for about 20 proteins. Most of the active genes on the Y chromosome are involved in sperm production and expressed only in the testes. Post-surgical transsexual women do not have testes.</p>
+
+<h3>Y chromosome genes active only in the testes or in sperm production</h3>
+		<ol>
+			<li> AZF1 - Involved in the production of sperm. As post-surgical transsexual women do not produce sperm, this gene has no function.</li>
+			<li> BPY2 - Expressed in cells in the testes. Again, since transsexual women do not have testes, this gene is not expressed.</li>
+			<li> DAZ1 - Involved in the production of sperm in the testes. Not expressed in post-surgical transsexual women.</li>
+			<li> DAZ2 - Similar to DAZ1. Not expressed in post-surgical transsexual women.</li>
+			<li> RBMY1A1 - Several copies of this gene are found on the Y chromosome, but most are inactive. This codes for a protein that researchers believe is involved in the production of sperm. Inactive in post-surgical transsexual women.</li>
+			<li> TGIF2LY - This gene produces a protein that is structurally similar to the protein produced by a gene on the X chromosome, but the Y variant seems to be active in the testis.</li>
+			<li> TSPY1 - Involved in sperm production and active only in the testes. Not expressed in post-surgical transsexual women.</li>
+			<li> USP9Y - Involved in sperm production and active only in the testes. Not expressed in post-surgical transsexual women.</li>
+		</ol>
+
+		<h3>Y chromosome genes with equivalents on other chromosomes (all humans have two copies; not unique to the Y chromosome)</h3>		
+		<ol>
+			<li> AMELX/Y - used in the development of tooth enamel. It also exists on the X chromosome.</li>
+			<li> CD24 - Involved in the differentiation of neurons and one type of immune system cell. There are copies on many other chromsomes, including chromosome 3, chromosome 20, and two locations on chromosome 15.<li>
+			<li> DDX3Y - While this gene does have a functional equivalent on the X chromosome, mutations in the gene affect the production of sperm, so it is likely active in the testes. Post-surgical transsexual women have no testes.</li>
+			<li> RPS4Y1/Y2/X - All of these genes code for the same ribosome protein S4. </li>
+			<li> SHOX - This gene is involved in the growth of the skeleton during development. It also exists on the X chromosome.</li>
+			<li> UTX/UTY - This gene codes for a protein that is involved in processes related to neurological development, and are expressed in adult brains as well. Interestingly, the UTY it also seems to have an immune response and has been linked to the rejection of stem cell grafts in XY people.</li>
+			<li> ZFX/ZFY - These genes are rough functional equivalents and seem to be involved in promoting the transcription of other genes.</li>
+		</ol>
+
+		<h3>Y chromosome genes that are inactive in both non-transsexual men and transsexual women</h3>
+		<ol>
+			<li> PRKY - This gene is transcribed but does not code for a functional protein, so it is functionally inactive even in men.</li>
+			<li> BCORP1 - This gene does not code for a functional protein, so it is inactive.</li>		
+		</ol>
 		
-		<p>Given this, it is very difficult to justify the claim that an XY person who has undergone removal of the testes, and has been on estrogen for several years, has any physiological advantages over an XX person. While they might be genetically male, the XY person is, physiologically, female.</p>
+		<h3>Y chromosome genes whose activity is unique to XY bodies</h3>
+		<ol>
+			<li> PCDH11Y - Involved in the development of the nervous system.</li>
+			<li> SRY - The big papa of the Y chromosome genes. As mentioned above, SRY is involved in the differentiation of embryos into anatomically male bodies. It sometimes gets moved to the X chromosome, leading to a number of interesting syndromes. Developmentally, it is primarily involved in the differentiation of gonads into testes during embryonic development, though it does seem to be expressed in the testes in adult XY humans. The Olympics used to test for the presence of the SRY gene to determine if an athlete could compete in women's divisions. They abandoned this practice in 2000 because it was not an accurate representation of athletic performance.</li>
+		</ol>
+		
+		<p>Given this, it is very difficult to justify the claim that an XY person who has undergone removal of the testes, and has been on estrogen for several years, has any physiological advantages over an XX person. While they might be genetically male, the XY person is, physiologically, female, and almost every Y chromosome gene is either not unique, inactive, .</p>
 		
 		<p>For more information on testosterone, see our information on <a href="hormones.php">hormones</a>, or on <a href="puberty.php">experiencing puberty under the influence of testosterone</a>.</p>
 		
