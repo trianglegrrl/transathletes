cd5818b467cb037d4921a9700df9b9c344202dc7
Alaina <alainahardie@gmail.com>
2013-04-15 09:10:45 -0500

    Tried to fix menu stuff

--- header.php
+++ header.php
@@ -18,6 +18,16 @@
     <script language="javascript" type="text/javascript" src="js/jquery.flot.candlestick.js"></script>
     <script language="javascript" type="text/javascript" src="js/jquery.flot.time.js"></script>
     <script language="javascript" type="text/javascript" src="js/bootstrap.min.js"></script>
+    <script language="javascript" type="text/javascript">
+			var url = window.location;
+			// Will only work if string in href matches with location
+			$('ul.nav a[href="'+ url +'"]').parent().addClass('active');
+
+			// Will also work for relative and absolute hrefs
+			$('ul.nav a').filter(function() {
+					return this.href == url;
+			}).parent().addClass('active');
+		</script>
     <style type="text/css">
     	body {
 /*     		height: 100%; */
